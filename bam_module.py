68b6d6605d200736ab60afcf94802bfaaafde71c
chelauk <chelauk@yahoo.co.uk>
2023-04-18 09:38:46 +0200

    added hash sum function

--- bam_module.py
+++ bam_module.py
@@ -1,6 +1,7 @@
 import os
 import re
 import pysam
+import hashlib
 
 
 def get_bam_samples(filename, **search_terms ):
@@ -9,6 +10,13 @@ def get_bam_samples(filename, **search_terms ):
             return [filename, {"aligner" : "bwa" }, {"comment" : search_terms["search2"]}]
         elif re.search( r"bam", filename ):
             return [filename, {"aligner" : "bwa"}, {"comment" : "bwa"} ]
+
+def calculate_md5sum(filename):
+    md5 = hashlib.md5()
+    with open(filename, 'rb') as f:
+        for chunk in iter(lambda: f.read(8192), b''):
+            md5.update(chunk)
+    return md5.hexdigest()
 
 
 def get_bams(folder,pattern,second_pattern):
@@ -25,9 +33,11 @@ def get_bams(folder,pattern,second_pattern):
                     location = root + "/" + my_item[0]
                     samples = pysam.samples(location)
                     sample = samples.split('\t')[0]
-                    actual_location = location.replace('../fht', '/group/sottoriva/00-PROCESSED_DATA/2022-MAYA_MISSONI_VALENTINO/pipeline/results/variant_calls')
+                    #md5sum = calculate_md5sum(location)
+                    actual_location = location.replace('/Volumes/', '/group/')
+                    project = actual_location.split('/')[4]
                     my_dict.update (
-                        { sample : { "file": { "location" : actual_location }, "type": my_item[1], 
-                                      "comment": my_item[2]}})
+                        { sample : { "file": { "location" : actual_location , "hash" : md5sum }, "type": my_item[1],
+                        "project": project, "comment": my_item[2]}})
 
     return(my_dict)
